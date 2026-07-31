4ad81813e8965dd5ac2f46118f4ad34a1fb7feb0
jeferson rodriguez <jjrodriguez1121@gmail.com>
2026-07-31 00:01:56 -0500

    viendo metodos

--- POO/Clases_y_Objetos.py
+++ POO/Clases_y_Objetos.py
@@ -4,5 +4,16 @@ class Celular:
         self.modelo = modelo
         self.camara = camara
 
+    def llamar(self):
+        print(f"Llamando desde el {self.modelo}")
 
-    
+    def colgar(self):
+        print(f"Colgando la llamada en el {self.modelo}")
+
+celular1 = Celular("Samsung", "Galaxy S21", "108MP")
+celular2 = Celular("Apple", "iPhone 13", "12MP")
+
+celular1.llamar()
+celular1.colgar()
+celular2.llamar()
+celular2.colgar()
