9b9dfe66d4d08ce62968bf96872a5b22a2118c76
jeferson rodriguez <jjrodriguez1121@gmail.com>
2026-08-01 18:06:35 -0500

    feat(Herencia): Enseña herencia multiple

--- POO/3.Herencia.py
+++ POO/3.Herencia.py
@@ -7,11 +7,30 @@ class Persona:
     def hablar(self):
         print(f"Hola, mi nombre es {self.nombre}, tengo {self.edad} años y soy de {self.nacionalidad}")
 
-class Empleado(Persona):
-    def __init__(self,nombre,edad,nacionalidad,puesto,salario):
-        super().__init__(nombre,edad,nacionalidad)
-        self.puesto = puesto
-        self.salario = salario
+class Artista:
+    def __init__(self,habilidad):
+        self.habilidad = habilidad
 
-roberto = Empleado("Roberto", 30, "Mexicano", "Ingeniero de Software", 50000)
-roberto.hablar() 
+    def mostrar_habilidad(self):
+        print(f"Mi habilidad es {self.habilidad}")
+
+class EmpleadoArtista(Persona, Artista):
+    def __init__(self, nombre, edad, nacionalidad, habilidad, salario, empresa):
+        Persona.__init__(self,nombre,edad,nacionalidad)
+        Artista.__init__(self,habilidad)
+        self.salario = salario
+        self.empresa = empresa
+
+    def mostrar_informacion(self):
+        print(f"Soy {self.nombre}, tengo {self.edad} años, soy de {self.nacionalidad}, mi habilidad es {self.habilidad}, gano {self.salario} y trabajo en {self.empresa}")
+ 
+
+
+
+
+
+
+roberto = EmpleadoArtista("Roberto", 30, "Mexicano", "Pintura", 50000, "Empresa X")
+roberto.hablar()
+roberto.mostrar_habilidad()
+roberto.mostrar_informacion()
